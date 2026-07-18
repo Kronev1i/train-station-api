fe3d743562bda7c9fd9fab899d3542a68d42cd18
unknown <kgoncharenko83@gmail.com>
2026-07-18 18:56:46 +0200

    feat: implement station models and migrations

---
+++ apps/station/models.py
@@ -0,0 +1,71 @@
+from django.db import models
+from django.conf import settings
+
+class Station(models.Model):
+    name = models.CharField(max_length=255)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
+class Route(models.Model):
+    source = models.ForeignKey(
+        Station,
+        on_delete=models.CASCADE,
+        related_name="route_source"
+    )
+    destination = models.ForeignKey(
+        Station,
+        on_delete=models.CASCADE,
+        related_name="route_destination"
+    )
+    distance = models.IntegerField()
+
+class TrainType(models.Model):
+    name = models.CharField(max_length=255)
+
+class Train(models.Model):
+    name = models.CharField(max_length=255)
+    cargo_num = models.IntegerField()
+    places_in_cargo = models.IntegerField()
+    train_type = models.ForeignKey(
+        TrainType,
+        on_delete=models.CASCADE
+    )
+
+class Crew(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+class Journey(models.Model):
+    route = models.ForeignKey(
+        Route,
+        on_delete=models.CASCADE
+    )
+    train = models.ForeignKey(
+        Train,
+        on_delete=models.CASCADE
+    )
+    departure_time = models.DateTimeField()
+    arrival_time = models.DateTimeField()
+    crew = models.ManyToManyField(
+        Crew,
+        related_name="journeys"
+    )
+
+class Order(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+
+class Ticket(models.Model):
+    cargo = models.IntegerField()
+    seat = models.IntegerField()
+    journey = models.ForeignKey(
+        Journey,
+        on_delete=models.CASCADE
+    )
+    order = models.ForeignKey(
+        Order,
+        on_delete=models.CASCADE
+    )
